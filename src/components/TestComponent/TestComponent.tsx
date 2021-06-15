1457eff0376faa0340b76b39178c1519f032d78e
Tiago Vale <tiago_oliveira914@hotmail.com>
2021-06-15 19:17:50 -0300

    disable class component from app and add useEffect hook

--- src/components/TestComponent/TestComponent.tsx
+++ src/components/TestComponent/TestComponent.tsx
@@ -1,9 +1,17 @@
-import React, { useState} from 'react'
+import React, { useState, useEffect} from 'react'
 import  './TestComponent.css'
 
 function TestComponent (props : {name:string}) {
     const [age, setAge] = useState(32);
-    
+
+    useEffect(()=>{
+        console.log("Component was created");
+    },[])
+
+    useEffect(()=>{
+        console.log('Age has been updated to ' + age)
+    },[age])
+
     return  <div className="TestComponent"> 
       Olá, {props.name}, {age}
       <button onClick={()=> {
