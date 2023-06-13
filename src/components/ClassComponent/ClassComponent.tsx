aca8f5e7ce51b105b663d112bc5298bf574d6a9a
Tiago Vale <tiago_oliveira914@hotmail.com>
2023-06-13 18:24:44 +0100

    lifecycle class component

--- src/components/ClassComponent/ClassComponent.tsx
+++ src/components/ClassComponent/ClassComponent.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 
 class ClassComponent extends React.Component<{name:String}>{
+    constructor(props:any){
+        super(props);
+        console.log('construct reached');
+    }
     state = {
         name:'mundo'
     }
+    componentDidMount(): void {
+        console.log('did mount reached');
+    }
+    componentDidUpdate(prevProps: Readonly<{ name: String; }>, prevState: Readonly<{}>, snapshot?: any): void {
+        console.log('did update reached');
+    }
     render(){
+        console.log('render reached');
         return <div>
             <p>Name: {this.state.name}</p>
             <button onClick={()=> {this.setState({
