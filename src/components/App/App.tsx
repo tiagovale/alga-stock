2c79523e9a92e09fe7555bbf92b79ddcdfb027cc
Tiago Vale <tiago_oliveira914@hotmail.com>
2023-06-21 12:03:39 +0100

    clear whole application

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,23 +1,11 @@
 import React from 'react';
 import './App.css';
-import TestComponent from '../TestComponent';
-import ClassComponent from '../ClassComponent/ClassComponent';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <TestComponent name="Tiago"/>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-         
-        </a>
-      </header>
+      App
     </div>
   );
 }
