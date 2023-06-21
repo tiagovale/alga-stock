fac7103a3d2375da4ff4925768192693018dfc56
Tiago Vale <tiago_oliveira914@hotmail.com>
2023-06-21 19:45:13 +0100

    clean app + add table component's base files

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,13 +1,7 @@
 import React  from 'react';
 import './App.css';
 import Header from '../Header';
-import Button from '../../shared/Button';
 import Container from '../../shared/Container/Container';
-import Input from '../../shared/Input';
-
-function TestComponent (){
-  return <img  width='50px' src='https://cdn.vectorstock.com/i/1000x1000/69/26/search-icon-magnifying-glass-vector-23086926.webp'></img>;
-}
 
 function App() {
   return (
@@ -15,15 +9,7 @@ function App() {
       <Header title='AlgaStock'/>
         <Container>
           {
-            <ul>
-              {
-                ['Tiago','Ana','Simão'].map((name,index)=>{
-                return <li key={index}>
-                  {name}
-                </li>
-                })
-              }
-            </ul>
+           
           }
         </Container>
       </div>
