55268cd79a774b21657921b06d702eb42b45a3f7
Tiago Vale <tiago_oliveira914@hotmail.com>
2023-06-21 11:56:16 +0100

    add useEffect hook

--- src/components/TestComponent/TestComponent.tsx
+++ src/components/TestComponent/TestComponent.tsx
@@ -1,15 +1,23 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import  './TestComponent.css';
 
 const TestComponent =(props:{name:string}) => {
  
-    const[age,setAge]= useState<number>(33);
+  const[age,setAge]= useState<number>(33);
 
+  useEffect(()=>{
+    console.log('Component was created');
+  },[])
   
+  useEffect(()=>{
+    console.log('Age has been updated to: ', age);
+  },[age])
 
   return  <div className='TestComponent'>
     Ola {props.name},{age}
-    <button onClick={()=> {setAge(age + 1)}}>Click me</button>
+    <button onClick={()=> {
+        setAge(age + 1)
+        }}>Click me</button>
     </div>
 }
 
