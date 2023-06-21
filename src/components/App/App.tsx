463ab244b4601fbc5b65f961145c413dcf246bb6
Tiago Vale <tiago_oliveira914@hotmail.com>
2023-06-21 19:37:15 +0100

    list render

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React  from 'react';
 import './App.css';
 import Header from '../Header';
 import Button from '../../shared/Button';
@@ -8,27 +8,23 @@ import Input from '../../shared/Input';
 function TestComponent (){
   return <img  width='50px' src='https://cdn.vectorstock.com/i/1000x1000/69/26/search-icon-magnifying-glass-vector-23086926.webp'></img>;
 }
-const [street, setStreet] = useState('');
 
 function App() {
   return (
     <div className="App">
       <Header title='AlgaStock'/>
         <Container>
-          <Button 
-            onClick={()=>{window.alert('asddasdsa')}}
-            appendIcon={<TestComponent/>}
-            >
-              Potato
-          </Button>
-
-          <Input
-            label='Street'
-            placeholder="E.g: 15th avenue"
-            value={street}
-            onChange={(e)=>{setStreet(e.target.value)}}
-          />
-
+          {
+            <ul>
+              {
+                ['Tiago','Ana','Simão'].map((name,index)=>{
+                return <li key={index}>
+                  {name}
+                </li>
+                })
+              }
+            </ul>
+          }
         </Container>
       </div>
   );
