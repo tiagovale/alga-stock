4dc7cf768acfed30c143b704908f1546527050dd
Tiago Vale <tiago_oliveira914@hotmail.com>
2023-06-21 19:50:12 +0100

    add table html structure

--- src/shared/Table/Table.tsx
+++ src/shared/Table/Table.tsx
@@ -2,8 +2,26 @@ import React from 'react'
 import './Table.scss'
 
 const Table = () => {
-    return <div>
-
-    </div>
+    return <table>
+        <thead>
+            <tr>
+                <th>Product</th>
+                <th>Price</th>
+                <th>Stock</th>
+            </tr>
+        </thead>
+        <tbody>
+             <tr>
+                <th>Cookie</th>
+                <th>$1.25</th>
+                <th>23</th>
+            </tr>
+            <tr>
+                <th>Milk 1L</th>
+                <th>$0.99</th>
+                <th>10</th>
+            </tr>
+        </tbody>
+    </table>
 }
 export default Table;
