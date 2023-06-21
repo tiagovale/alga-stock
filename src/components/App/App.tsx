f550971aabeae6697d3035d48ecd8fd54a20755e
Tiago Vale <tiago_oliveira914@hotmail.com>
2023-06-21 19:24:22 +0100

    add input component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Header from '../Header';
 import Button from '../../shared/Button';
 import Container from '../../shared/Container/Container';
+import Input from '../../shared/Input';
 
 function TestComponent (){
   return <img  width='50px' src='https://cdn.vectorstock.com/i/1000x1000/69/26/search-icon-magnifying-glass-vector-23086926.webp'></img>;
 }
+const [street, setStreet] = useState('');
 
 function App() {
   return (
@@ -19,8 +21,16 @@ function App() {
             >
               Potato
           </Button>
+
+          <Input
+            label='Street'
+            placeholder="E.g: 15th avenue"
+            value={street}
+            onChange={(e)=>{setStreet(e.target.value)}}
+          />
+
         </Container>
-        </div>
+      </div>
   );
 }
 
